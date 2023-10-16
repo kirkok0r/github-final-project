a84489b7ed28146c7516c2b26c661b96351f2ab3
Kiril Korneienkov <87307837+kirkok0r@users.noreply.github.com>
2023-10-16 15:31:39 +0300

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -4,7 +4,7 @@
    # Do not use this in production. Sample purpose only.
    # Author: Upkar Lidder (IBM)
    # Additional Authors:
-   # kirkok0r
+   kirkok0r
    # Input:
    # p, principal amount
    # t, time period in years
